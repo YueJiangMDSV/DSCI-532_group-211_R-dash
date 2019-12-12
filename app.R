0bcc5e0d6027bbecd721cea982843e4b3fd07ae0
Arun Marria <arun.marria@gmail.com>
2019-12-11 16:18:02 -0800

    tab3 and tab 1 added with table and charts

--- app.R
+++ app.R
@@ -6,7 +6,7 @@ library(dashTable)
 library(tidyverse)
 library(plotly)
 library(ggplot2)
-library(tidyverse)
+# library(tidyverse)
 library(readxl)
 library("cowplot")
 library("gapminder")
@@ -26,7 +26,26 @@ app <- Dash$new(external_stylesheets = "https://codepen.io/chriddyp/pen/bWLwgP.c
 # importing wrangled dataset
 
 df <- read_csv("data/tab1.csv")
+df2 <- read_csv("data/tab2.csv")
 df$company <- factor(df$company)
+
+# Dataset intro table
+table <- dashDataTable(
+            id='data-table',
+              style_table = list(
+              maxHeight = '200',
+              overflowY = 'scroll'
+            ),
+          columns = map(colnames(df), 
+                   function(colName){
+                     list(
+                       id = colName,
+                       name = colName
+                     )
+                   }),
+          data=df[c(1,2,3,123,124,125,246,247,248,369,370,371,437,438,439), ]    
+
+                )
 
 # stock history plot
 make_graph1 <- function(df){
@@ -61,6 +80,23 @@ plot2_tab1 <- df %>%
                        }
 
 
+  #plot for tab3
+
+  make_graph3 <- function(df2){
+
+
+    plot_tab2 <-  df2 %>%
+        ggplot(aes(x = date, y = inv_value, group = company, color = company)) +
+        geom_line() +
+        ggtitle("Investment value from August 2004 to March 2010") +
+        labs(x = "Date",
+            y = "Investment Value $USD")
+            ggplotly(plot_tab2)
+  }
+
+
+
+
 # Now we define the graph as a dash component using generated figure
 graph1 <- dccGraph(
   id = 'history-graph',
@@ -72,6 +108,10 @@ graph2 <- dccGraph(
   figure=make_graph2(df) # gets initial data using argument defaults
 )
 
+graph3 <- dccGraph(
+  id = 'investment-graph',
+  figure=make_graph3(df2) # gets initial data using argument defaults
+)
 # stock companies' dropdown
 
 stocksDropdown <- dccDropdown(
@@ -87,6 +127,38 @@ stocksDropdown <- dccDropdown(
 
 
 
+## slider for tab3
+dates <- c(
+         '2004-08-01', '2005-02-01', '2005-08-01', '2006-02-01', '2006-08-01',
+         '2007-02-01',  '2007-08-01','2008-02-01',  '2008-08-01','2009-02-01',  '2009-08-01',
+         '2010-02-01', '2010-03-01')
+# date_mark = {i : dates[i] for i in range(0,13)}
+
+yearMarks <- map(dates, as.character)
+# names(yearMarks) <- dates
+yearSlider <- dccRangeSlider(
+  id = "year",
+  marks = yearMarks,
+  min = 0,
+  max = 12,
+ 
+  value = list(0,12)
+)
+
+#  html.P([
+#                     html.Label("Year range"),
+#                     dcc.Slider(id = 'slider',
+#                                     marks = date_mark,
+#                                     min = 0,
+#                                     max = 12,
+#                                     value = 10) 
+#                         ], style = {'width' : '75%',
+#                                     'fontSize' : '20px',
+#                                     'padding-left' : '220px',
+#                                     'display': 'inline-block',}),
+
+
+
 
 app$layout(
   htmlDiv(
@@ -94,7 +166,29 @@ app$layout(
       htmlH1("Stock Price Data Analysis"),
       dccTabs(id="tabs-example", value='tab-1-example',children=list(
 
-    dccTab(label = "About our data"), 
+        # tab1 for data intro
+    dccTab(label = "About our data", htmlDiv(list(
+      htmlH1("Dataset Introduction"),
+      htmlP("The dataset we are using is the  Stocks  data from the vega-datasets. 
+            The dataset has 560 observations in total. "),
+
+                # addding dataset intro table to tab1
+            table,
+
+            
+            htmlP("There are 5 companies in total, and they are Microsoft, Amazon, IBM, Google, and Apple."),
+            htmlP("The date column lists out the date when the stock price was recorded.
+            The value of the date column ranges from January 1, 2000 to March 1, 2010. The date range is the same for Microsoft, Amazon, IBM, and Apple. Each of them has 123 observations in the dataset. 
+            Since Google held its IPO in August, 2004, the record for Google started from August 1, 2004. 
+            Therefore, there are 68 observations for Google."),
+            htmlP(" The price column lists out the price of that stock on the recorded date."),
+            htmlP(" The purpose of this app is to help people form a better view of stock price fluctuations 
+                        and long-term investment gains.")
+   
+            )
+            )), 
+
+    # tab 2
     dccTab(label='Stock trends', htmlDiv(list(
         htmlH1("Price History"),
                 htmlH2("From 2000 to 2010, Apple's stock price increased 760%." ),
@@ -110,9 +204,51 @@ app$layout(
 
 
     ),
-    dccTab(label = "Investment Value")
-    
+    dccTab(label = "Investment Value" ,htmlDiv(list(
+        htmlH1("How much will I gain?"),
+        htmlH3("If I invested $10,000 in one of the companies in August 2004, how much will my investment worth in later days for each company?"),
+        htmlP("Use the year slider bar to select the time range and find out the investment value."), 
+                       
+            
+    # range slider for selecting time range
+        htmlP(yearSlider),
+  
+            
+            # To add some space
+              
+              htmlIframe(height=15, width=10, style=list(borderWidth = 0)), #space
+            # adding investment plot
 
+            graph3,
+             
+                htmlH1("Why Apple has the highest investment value?"),
+                htmlP("If you are curious why Apple has the highest investment value while Google has the highest price in the historial price chart you have seen in the previous tab, let's see the math here."), 
+                     
+                htmlP("In August 2004, Google's stock price was $102.37. With $10,000, I can buy 10,000/102.37 = 97.68 shares."), 
+                    
+                htmlP("In March 2010, Google's stock price was $560.19. Then my total investment value is 560.19 * 97.68 shares = $54,722.08."), 
+                    
+                htmlP("On the other hand, in August 2004, Apple's stock price was $17.25. With $10,000, I can buy 10,000/102.37 = 579.71 shares."), 
+                    
+                htmlP("In March 2010, Apple's stock price was $223.02. Then my total investment value is  223.02 579.71 * shares = $129,286.95."), 
+                   
+                htmlH4("Clearly, $129,286.95 worth much more than $54,722.08. You would earn much more if you picked Apple!"), 
+                       
+                htmlH3("Between 2004 and 2010, Google's stock price only increased 447.91%, but Apple increased 1192.9%."), 
+                    
+                htmlP("It is this high growth that drags up Apple's investment value."), 
+                    
+                htmlH2("In investment, growth is more important than price!"), 
+                    
+                htmlP("Hope this answers your question and gives you some insights on investment."), 
+                    
+                
+                # To add some space
+                htmlIframe(height=15, width=10, style=list(borderWidth = 0)) #space
+
+
+    )
+    ))
 )
 )
 )
